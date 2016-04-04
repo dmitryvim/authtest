43ca3645a5ba3e64f54e9eef4b94f835ec182910
Dmitry Mikhaylovich <dmitry.mikhailovich@gmail.com>
2016-04-04 15:28:16 +0300

    test step

--- src/main/java/ru/mikhaylovich/start/StartpageController.java
+++ src/main/java/ru/mikhaylovich/start/StartpageController.java
@@ -49,10 +49,10 @@ public class StartpageController {
 
         OAuth2AccessToken accessToken = service.getAccessToken(code);
         final OAuthRequest request = new OAuthRequest(Verb.GET, "https://api.hh.ru/me", service);
-        service.signRequest(accessToken, request);
-        final Response response = request.send();
-        redirectAttributes.addAttribute("name", response.getBody());
-        //redirectAttributes.addAttribute("name", code);
+//        service.signRequest(accessToken, request);
+//        final Response response = request.send();
+//        redirectAttributes.addAttribute("name", response.getBody());
+        redirectAttributes.addAttribute("name", code);
 
         return new RedirectView("/greeting");
     }
